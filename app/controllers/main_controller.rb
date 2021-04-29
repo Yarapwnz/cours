1677edee19bf97111d23fa072aae4d686dd04668
yarapwnz <yarabarabaluk@gmail.com>
2021-04-29 22:09:47 +0300

    firstCommit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,7 @@ class MainController < ApplicationController
   before_action :set_page_options
   def index
     @brands = Brand.limit(3)
-    @hits   = Product.limit(8)
+    @hits  = Product.limit(8)
   end
 
   def set_page_options
